40badd6f3fcd22b13159938ab6a0c9e16c89a18f
Sasmira <sasmyra@gmail.com>
2020-06-24 23:26:53 +0000

    Update module/css/cursor.css, module/css/pathfinderui.css, module/scripts/dicefinder.js, module/scripts/pathfinder-chat-color.js, module/ui/buttons/button-blue.png, module/ui/buttons/button-green.png, module/ui/buttons/button-grey.png, module/ui/buttons/button-red.png, module/ui/frames/Chat-box.png, module/ui/frames/chat-controls-border.png, module/ui/frames/hotkeybox.png, module/ui/frames/hotkeybox-macro-list.png, module/ui/frames/players-border.png, module/ui/frames/chat-texture.png, module/ui/sidebar/sidebar_button-actors.png, module/ui/sidebar/sidebar_button-background.png, module/ui/sidebar/sidebar_button-background_cyan.png, module/ui/sidebar/sidebar_button-background_green.png, module/ui/sidebar/sidebar_button-background_grey.png, module/ui/sidebar/sidebar_button-background_red.png, module/ui/sidebar/sidebar_button-chat.png, module/ui/sidebar/sidebar_button-combat-tracker.png, module/ui/sidebar/sidebar_button-compendium.png, module/ui/sidebar/sidebar_button-footer-button.png, module/ui/sidebar/sidebar_button-journal.png, module/ui/sidebar/sidebar_button-object.png, module/ui/sidebar/sidebar_button-playlist.png, module/ui/sidebar/sidebar_button-rolltable.png, module/ui/sidebar/sidebar_buttons.png, module/ui/sidebar/sidebar_button-scenes.png, module/ui/sidebar/sidebar_button-settings.png, module/module.json files Deleted module/ui/buttons/buttonbluelight.png, module/ui/buttons/buttonbluelight2.png, module/ui/buttons/buttonbluelight3.png, module/ui/buttons/buttongreenlight.png, module/ui/buttons/buttongreylight.png, module/ui/buttons/buttonlight3.png, module/ui/buttons/buttonredlight.png, module/ui/frame/Chatbox.png, module/ui/frame/chat-controls-border - Copie.png, module/ui/frame/chat-controls-border.png, module/ui/frame/chattexture.png, module/ui/sidebar/actors.png, module/ui/sidebar/chat.png, module/ui/sidebar/combat-tracker.png, module/ui/sidebar/compendium.png, module/ui/sidebar/journal.png, module/ui/sidebar/object.png, module/ui/sidebar/playlist.png, module/ui/sidebar/rolltable.png, module/ui/sidebar/scenes.png, module/ui/sidebar/settings.png files

--- module/scripts/dicefinder.js
+++ module/scripts/dicefinder.js
@@ -1,5 +1,5 @@
 Hooks.on('diceSoNiceReady', (dice3d) => {
-    dice3d.addSystem({id: "PF", name: "Pathfinder"}, true);
+    dice3d.addSystem({id: "PF12", name: "Pathfinder 1 & 2"}, true);
   
     dice3d.addDicePreset({
       type: "d20",
@@ -25,24 +25,24 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
         "19",
         "modules/pathfinder-ui/ui/dice/nat20.png"
       ],
-      system: "PF"
+      system: "PF12"
     });
   
-    dice3d.addTexture("PFred", {
+    dice3d.addTexture("PF12red", {
       name: "Pathfinder Red",
       composite: "source-over",
       source: "modules/pathfinder-ui/ui/dice/redTexture.png"
     })
     .then(() => {
         dice3d.addColorset({
-          name: 'pf',
+          name: 'pf12',
           description: "Pathfinder Red/Gold",
-          category: "Pathfinder",
-          texture: 'PFred',
+          category: "Pathfinder 1 & 2",
+          texture: 'PF12red',
           edge: '#d9a463',
           foreground: '#d9a463',
           default: true
-        },"default");
+        },"force");
     });
   });
   
