8c57f58b5c7797974a88d50b4cc0540ef21149fd
Sasmira <sasmyra@gmail.com>
2023-08-15 15:30:45 +0200

    ## v11.307.8
    ## v11.307.8
    - Update interface code.
    - Fix error when Monks little details wasn't activate.
    - Update token hud theme.
    - Update Compatibility with Monks little details.

--- module/scripts/rpg-ui.js
+++ module/scripts/rpg-ui.js
@@ -1,8 +1,10 @@
 Hooks.on('ready', async () => {
-	// Retrait de la classe de Monk's Little Details
-	// Créer une erreur lorsque le module n'est pas activé
-	game.settings.set("monks-little-details", "window-css-changes", false);
-	$("body").removeClass("change-windows");
+    // Retrait de la classe de Monk's Little Details
+    // Créer une erreur lorsque le module n'est pas activé
+        if (game.settings.settings.has('monks-little-details.window-css-changes')) {
+                game.settings.set("monks-little-details", "window-css-changes", false);
+                $("body").removeClass("change-windows");
+        }
 });
 
 Hooks.on('init', () => {
