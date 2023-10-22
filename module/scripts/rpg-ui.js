8ced3bebe75ae506cfded66eb0bde47fddd64e70
Vivien DE BONA <vdebona@umanit.fr>
2023-10-22 12:27:17 +0200

    feat: add Roll Buttons from DragonFlagon Chat Enhancements
    - Get code from DragonFlagon module as the module is not maintained

--- module/scripts/rpg-ui.js
+++ module/scripts/rpg-ui.js
@@ -1,3 +1,5 @@
+import ChatRollPrivacy from './chat-roll-privacy.js';
+
 Hooks.on('ready', async () => {
     // Retrait de la classe de Monk's Little Details
     // Créer une erreur lorsque le module n'est pas activé
@@ -166,6 +168,12 @@ Hooks.on('init', () => {
 	if (!game.settings.get('pathfinder-ui', 'journalSheet')) { rpgUIAddJournalSheet() }
 	if (!game.settings.get('pathfinder-ui', 'cursor')) { rpgUIAddCursor() }
 	if (game.settings.get('pathfinder-ui', 'minimalUICompatibility')) { addClassByQuerySelector('minimal-ui-mode', 'body.vtt') }
+
+	ChatRollPrivacy.init();
+});
+
+Hooks.once('setup', function () {
+	ChatRollPrivacy.setup();
 });
 
 Hooks.on('getSceneNavigationContext', () => {
